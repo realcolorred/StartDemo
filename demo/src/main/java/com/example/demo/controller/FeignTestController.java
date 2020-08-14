7b0ee7c2c9262102807feb1b8baa852f44ebb6aa
realcolorred <757124271@qq.com>
2020-08-14 09:29:06 +0800

    1

--- demo/src/main/java/com/example/demo/controller/FeignTestController.java
+++ demo/src/main/java/com/example/demo/controller/FeignTestController.java
@@ -1,22 +1,29 @@
 package com.example.demo.controller;
 
-import com.example.demo.feignapi.TestFeign;
+import com.example.demoapi.api.TestFeign;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zengli
  */
+@Api(description = "feign调用测试")
 @RestController
 @RequestMapping("/feign")
 public class FeignTestController {
 
-    @Autowired(required = false)
+    //@Autowired(required = false)
+    @Autowired
     private TestFeign testFeign;
 
-    @RequestMapping("test")
-    public String test() {
-        return testFeign.hello();
+    @ApiOperation("测试")
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return testFeign.query();
     }
 }
